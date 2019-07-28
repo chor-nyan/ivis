6d3e948854080e497aa1aad1fcd6d0684bacd22a
chor-nyan <chor.nyan39@gmail.com>
2019-07-28 18:36:49 +0900

    change codes

--- examples/mnist.py
+++ examples/mnist.py
@@ -8,6 +8,8 @@ embeddings.
 """
 
 import os
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
 from sklearn.datasets import fetch_openml
@@ -15,7 +17,7 @@ from ivis.ivis import Ivis
 
 mnist = fetch_openml('mnist_784', version=1)
 ivis = Ivis(model='maaten', verbose=1)
-n = 70000
+n = 10000
 data = mnist.data[:n, :]
 embeddings = ivis.fit_transform(data)
 
@@ -24,9 +26,9 @@ color = mnist.target[:n].astype(int)
 
 plt.figure(figsize=(8, 8), dpi=150)
 plt.scatter(x=embeddings[:, 0],
-            y=embeddings[:, 1], c=color, cmap="Spectral", s=0.1)
+            y=embeddings[:, 1], c=color, cmap="Spectral", s=1)
 plt.xlabel('ivis 1')
 plt.ylabel('ivis 2')
 plt.show()
-
+plt.savefig('figure.png')
 os.remove('annoy.index')
